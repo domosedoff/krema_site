1f85b08032575f37251dd069f4ab52cc6056341a
Yuri.P <domosedov@mail.ru>
2023-06-05 03:50:49 +0300

    05-06-2023

---
+++ server/src/moks/index.ts
@@ -0,0 +1,208 @@
+export const users = [
+  {
+    _id: '647d12d7359989f9757b3491',
+    name: 'Deann Burgess',
+    gender: 'female',
+    email: 'deannburgess@insurity.com',
+    phone: '+7 (930) 532-3817',
+    about:
+      'Enim labore et sunt aute et adipisicing reprehenderit deserunt dolore dolore ea eu. Cillum consectetur exercitation nostrud eu quis officia duis nulla non aute laborum reprehenderit. Deserunt pariatur reprehenderit dolor commodo nostrud sint minim do occaecat adipisicing dolore nulla. Aute Lorem excepteur est exercitation consectetur eiusmod do dolore exercitation aliqua duis nulla. Deserunt ea cillum irure est commodo fugiat reprehenderit ullamco laboris nisi id qui qui. Nisi veniam nostrud eu ipsum elit. Amet laboris consectetur esse cillum nulla nostrud dolore irure adipisicing aute Lorem aliqua aliqua exercitation.\r\n',
+    registered: '2021-01-09T02:04:46 -03:00',
+    tags: [
+      'nostrud',
+      'incididunt',
+      'veniam',
+      'non',
+      'velit',
+      'fugiat',
+      'proident',
+    ],
+    friends: [
+      {
+        id: 0,
+        name: 'Waller Mosley',
+      },
+      {
+        id: 1,
+        name: 'Keisha Turner',
+      },
+      {
+        id: 2,
+        name: 'Marian Everett',
+      },
+    ],
+    greeting: 'Hello, Deann Burgess! You have 9 unread messages.',
+  },
+  {
+    _id: '647d12d758bfd71e03982b05',
+    name: 'Marcy Moss',
+    gender: 'female',
+    email: 'marcymoss@insurity.com',
+    phone: '+7 (845) 460-2425',
+    about:
+      'Adipisicing magna exercitation elit qui ipsum amet enim dolor. Ullamco laborum magna sint sit pariatur Lorem sunt. Cupidatat ut ex elit ut adipisicing enim ipsum aliqua dolore laboris sint esse velit. Do Lorem exercitation aute et ullamco officia aute incididunt aliquip excepteur et id do ut. Lorem aliquip consectetur incididunt adipisicing. Mollit culpa minim commodo veniam cillum voluptate mollit incididunt. Nisi nisi tempor eu occaecat.\r\n',
+    registered: '2015-03-13T12:22:03 -03:00',
+    tags: ['amet', 'labore', 'cupidatat', 'esse', 'enim', 'et', 'ipsum'],
+    friends: [
+      {
+        id: 0,
+        name: 'Giles Battle',
+      },
+      {
+        id: 1,
+        name: 'Davidson Deleon',
+      },
+      {
+        id: 2,
+        name: 'Workman Grimes',
+      },
+    ],
+    greeting: 'Hello, Marcy Moss! You have 3 unread messages.',
+  },
+  {
+    _id: '647d12d77966b75e4d4ce93b',
+    name: 'Sybil Odom',
+    gender: 'female',
+    email: 'sybilodom@insurity.com',
+    phone: '+7 (991) 455-3997',
+    about:
+      'Dolore duis exercitation duis dolore veniam. Occaecat laboris ut proident fugiat occaecat occaecat est amet tempor non velit excepteur anim. Nulla exercitation reprehenderit aute officia aute ullamco laboris non dolor id laboris nostrud do et.\r\n',
+    registered: '2023-02-01T07:08:28 -03:00',
+    tags: ['ipsum', 'enim', 'commodo', 'sint', 'quis', 'aliquip', 'eiusmod'],
+    friends: [
+      {
+        id: 0,
+        name: 'Farmer Rodriquez',
+      },
+      {
+        id: 1,
+        name: 'Osborn Hernandez',
+      },
+      {
+        id: 2,
+        name: 'Esmeralda Warren',
+      },
+    ],
+    greeting: 'Hello, Sybil Odom! You have 2 unread messages.',
+  },
+  {
+    _id: '647d12d7f96460604859a2b6',
+    name: 'Frances Strong',
+    gender: 'female',
+    email: 'francesstrong@insurity.com',
+    phone: '+7 (912) 466-3824',
+    about:
+      'Aute nulla mollit consequat aute voluptate Lorem. Eu laboris sunt cillum mollit pariatur pariatur. Consectetur deserunt velit pariatur quis qui.\r\n',
+    registered: '2016-11-22T07:30:41 -03:00',
+    tags: ['nulla', 'ex', 'veniam', 'reprehenderit', 'id', 'adipisicing', 'id'],
+    friends: [
+      {
+        id: 0,
+        name: 'Golden Mcgee',
+      },
+      {
+        id: 1,
+        name: 'Glover Cameron',
+      },
+      {
+        id: 2,
+        name: 'Knapp Petersen',
+      },
+    ],
+    greeting: 'Hello, Frances Strong! You have 6 unread messages.',
+  },
+  {
+    _id: '647d12d7fd2b9d3b90dd4f49',
+    name: 'Mckay Soto',
+    gender: 'male',
+    email: 'mckaysoto@insurity.com',
+    phone: '+7 (806) 446-2852',
+    about:
+      'Ut ullamco enim magna magna consectetur amet. Labore ad do ullamco duis ex occaecat duis quis eu labore aliqua labore nisi. Cupidatat in et do ut sunt quis irure eu. Exercitation amet consectetur nostrud dolor occaecat fugiat magna. Proident Lorem reprehenderit incididunt eu ex. Eiusmod id ipsum nulla nostrud eu cupidatat pariatur fugiat incididunt. Sit est incididunt anim nostrud dolor.\r\n',
+    registered: '2015-10-27T12:36:18 -03:00',
+    tags: ['elit', 'dolore', 'magna', 'in', 'laboris', 'ea', 'veniam'],
+    friends: [
+      {
+        id: 0,
+        name: 'Janna Munoz',
+      },
+      {
+        id: 1,
+        name: 'Alexander Wallace',
+      },
+      {
+        id: 2,
+        name: 'Ruthie Sims',
+      },
+    ],
+    greeting: 'Hello, Mckay Soto! You have 10 unread messages.',
+  },
+  {
+    _id: '647d12d743cebe45dd6fc477',
+    name: 'Avis Perkins',
+    gender: 'female',
+    email: 'avisperkins@insurity.com',
+    phone: '+7 (973) 489-3505',
+    about:
+      'Commodo dolore amet incididunt voluptate ullamco aliqua do duis cillum velit consectetur deserunt veniam. Magna commodo deserunt culpa fugiat ullamco pariatur deserunt enim eu laboris consequat ea veniam. In quis magna minim qui velit nostrud proident minim incididunt consectetur ex.\r\n',
+    registered: '2020-07-21T02:26:49 -03:00',
+    tags: [
+      'consequat',
+      'cupidatat',
+      'eiusmod',
+      'incididunt',
+      'Lorem',
+      'qui',
+      'fugiat',
+    ],
+    friends: [
+      {
+        id: 0,
+        name: 'Nadine Preston',
+      },
+      {
+        id: 1,
+        name: 'Elsa Chandler',
+      },
+      {
+        id: 2,
+        name: 'Marquita Duke',
+      },
+    ],
+    greeting: 'Hello, Avis Perkins! You have 10 unread messages.',
+  },
+  {
+    _id: '647d12d762efd0fa3954f9b3',
+    name: 'Carlene Mcclain',
+    gender: 'female',
+    email: 'carlenemcclain@insurity.com',
+    phone: '+7 (804) 539-2944',
+    about:
+      'Mollit cupidatat excepteur incididunt ea ea duis laborum ex ea esse laboris. Occaecat anim consequat ullamco ut anim ipsum pariatur aliquip nisi minim. Deserunt magna dolor occaecat aliquip cupidatat velit. Lorem nisi dolor dolore est amet. Voluptate laborum esse aute cupidatat quis mollit Lorem excepteur ad qui adipisicing officia deserunt excepteur. Incididunt consequat eiusmod est voluptate dolor nostrud eiusmod cillum.\r\n',
+    registered: '2018-09-07T09:32:48 -03:00',
+    tags: [
+      'aliquip',
+      'ea',
+      'incididunt',
+      'voluptate',
+      'enim',
+      'cupidatat',
+      'fugiat',
+    ],
+    friends: [
+      {
+        id: 0,
+        name: 'Buckley Sawyer',
+      },
+      {
+        id: 1,
+        name: 'Ladonna Bass',
+      },
+      {
+        id: 2,
+        name: 'Vickie Ryan',
+      },
+    ],
+    greeting: 'Hello, Carlene Mcclain! You have 8 unread messages.',
+  },
+];
